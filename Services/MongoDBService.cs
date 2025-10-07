61703a2e36f9333f44f72d12c9e49a2b1d1e6d8d
Jesus Bejar <israelbejarii@gmail.com>
2025-10-07 10:07:56 -0600

    added auth crud ops

---
+++ Services/MongoDBService.cs
@@ -0,0 +1,51 @@
+using MongoDB.Driver;
+using RestaurantMS.Models;
+
+namespace RestaurantMS.Services
+{
+    public class MongoDBService
+    {
+        private readonly IMongoDatabase _database;
+        private readonly IMongoCollection<User> _users;
+
+        public MongoDBService(IConfiguration configuration)
+        {
+            var connectionString = configuration.GetConnectionString("MongoDB");
+            var client = new MongoClient(connectionString);
+            _database = client.GetDatabase("RestaurantMS");
+            _users = _database.GetCollection<User>("Users");
+        }
+
+        // User operations
+        public async Task<List<User>> GetUsersAsync()
+        {
+            return await _users.Find(user => true).ToListAsync();
+        }
+
+        public async Task<User?> GetUserByIdAsync(string id)
+        {
+            return await _users.Find(user => user.Id == id).FirstOrDefaultAsync();
+        }
+
+        public async Task<User?> GetUserByEmailAsync(string email)
+        {
+            return await _users.Find(user => user.Email == email).FirstOrDefaultAsync();
+        }
+
+        public async Task CreateUserAsync(User user)
+        {
+            await _users.InsertOneAsync(user);
+        }
+
+        public async Task UpdateUserAsync(string id, User user)
+        {
+            await _users.ReplaceOneAsync(u => u.Id == id, user);
+        }
+
+        public async Task DeleteUserAsync(string id)
+        {
+            await _users.DeleteOneAsync(user => user.Id == id);
+        }
+
+    }
+}
